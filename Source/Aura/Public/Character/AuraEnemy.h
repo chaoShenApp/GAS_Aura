5bc7e70d7acf086a53964fc56566864e2d5ef772
steven zhang <stevenzhang56@126.com>
2024-08-12 23:32:35 +0800

    init ability actor info

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,10 +16,13 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
-	
-public:
+
+	//~ Begin Enemy Interface 
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
-
+	/** end Enemy Interface */
+	
+protected:
+	virtual void BeginPlay() override;
 
 };
